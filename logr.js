844d119cf9b051ed7c4fc7a6a2003147cb6be298
Julien Castelain <jcastelain@gmail.com>
2013-07-05 23:38:08 +0200

    Adding files

---
+++ logr.js
@@ -0,0 +1,43 @@
+/*jslint indent: 2*/
+(function (exports) {
+  'use strict';
+
+  var logr,
+    levels,
+    xhr,
+    hasConsole;
+
+  levels = ['log', 'debug', 'error', 'info', 'warn'];
+
+  hasConsole = function (obj, fn) {
+    return (!!obj.console && typeof obj.console[fn] === 'function');
+  };
+
+  if (typeof exports.logr === 'undefined') {
+    logr = exports.logr = function (opts) {
+      var level, transport;
+      opts = opts || {};
+
+      level = levels[opts.level] || levels[0];
+      transport = opts.transport;
+
+      return function (message) {
+        message = (typeof message === 'string' && !!message) ? message : '';
+
+        return function () {
+          var args = [message].concat([].slice.call(arguments, 0));
+
+          if (typeof transport === 'function') {
+
+            transport.apply(null, args);
+          
+          } else if (hasConsole(exports, level)) {
+            exports.console[level].apply(exports.console, args);
+          }
+          return exports.logr;
+        };
+      };
+    };
+  }
+
+}(this));
